23941ec3b612ef63db14a82e98f146a510ebef58
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-07-19 11:33:06 -0300

    Aula 4.3

--- InstaluraMobile/src/components/Post.js
+++ InstaluraMobile/src/components/Post.js
@@ -7,14 +7,34 @@
  */
 
 import React, { useState } from "react";
-import { StyleSheet, View, Image, Dimensions, Text, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Image,
+  Dimensions,
+  Text,
+  TouchableOpacity
+} from "react-native";
 import emptyHeartImg from "../../resources/img/s2.png";
 import likedImg from "../../resources/img/s2-checked.png";
 
 const Post = props => {
   const [foto, setFoto] = useState(props.foto);
 
-  const loadLikeImage = (liked) => liked ? likedImg : emptyHeartImg;
+  const loadLikeImage = liked => (liked ? likedImg : emptyHeartImg);
+
+  const renderLikesCount = () =>
+    foto.likers.length > 0 ? (
+      <Text style={styles.likes}>{foto.likers.length} curtidas</Text>
+    ) : null;
+
+  const renderComment = () =>
+    foto.comentario !== "" ? (
+      <View style={styles.comment}>
+        <Text style={styles.commentTitle}>{foto.loginUsuario}</Text>
+        <Text>{foto.comentario}</Text>
+      </View>
+    ) : null;
 
   return (
     <View>
@@ -24,9 +44,13 @@ const Post = props => {
       </View>
       <Image source={{ uri: foto.urlFoto }} style={styles.postImg} />
       <View style={styles.footer}>
-        <TouchableOpacity onPress={() => setFoto({...foto, likeada: !foto.likeada})}>
+        <TouchableOpacity
+          onPress={() => setFoto({ ...foto, likeada: !foto.likeada })}
+        >
           <Image source={loadLikeImage(foto.likeada)} style={styles.likeImg} />
         </TouchableOpacity>
+        {renderLikesCount()}
+        {renderComment()}
       </View>
     </View>
   );
@@ -49,11 +73,22 @@ const styles = StyleSheet.create({
     width: Dimensions.get("screen").width
   },
   likeImg: {
+    marginBottom: 10,
     width: 40,
     height: 40
   },
   footer: {
     margin: 10
+  },
+  likes: {
+    fontWeight: "bold"
+  },
+  comment: {
+    flexDirection: "row"
+  },
+  commentTitle: {
+    fontWeight: "bold",
+    marginRight: 5
   }
 });
 
