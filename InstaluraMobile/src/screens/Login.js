b93ff400f0f1e491414be58328e8e5ebca4f3f11
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-07-22 13:40:22 -0300

    2.2

--- InstaluraMobile/src/screens/Login.js
+++ InstaluraMobile/src/screens/Login.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
-import { View, TextInput, StyleSheet, Dimensions, Button, Text } from "react-native";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  Button,
+  Text
+} from "react-native";
 
 const width = Dimensions.get("screen").width;
 
 class Login extends Component {
+    state = {
+        usuario: "",
+        senha: ""
+    }
+
+  login = () => {
+    const requestInfo = {
+      method: "POST",
+      body: JSON.stringify({
+        login: this.state.usuario,
+        senha: this.state.senha
+      }),
+      headers: {
+          "Content-type": "application/json"
+      }
+    };
+
+    fetch("https://instalura-api.herokuapp.com/api/public/login", requestInfo)
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        }
+        throw new Error("Não foi possível efetuar login");
+      })
+      .then(token => console.warn(token));
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -13,14 +47,16 @@ class Login extends Component {
             placeholder="Usuário..."
             onChangeText={text => this.setState({ usuario: text })}
             style={styles.input}
-          />
+            autoCapitalize="none"
+            />
 
           <TextInput
             placeholder="Senha..."
             onChangeText={text => this.setState({ senha: text })}
             style={styles.input}
+            secureTextEntry={true}
           />
-          <Button title="Login" onPress={() => console.warn("bah!")} />
+          <Button title="Login" onPress={this.login} />
         </View>
       </View>
     );
